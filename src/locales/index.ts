fbf7b2c05b26f33c78c1c8eddd007dbe175595b2
tiby <101003754+0xtiby@users.noreply.github.com>
2023-06-18 12:22:00 +0200

    Setup shadcn ui and locales

---
+++ src/locales/index.ts
@@ -0,0 +1,6 @@
+export const locales = {
+  appName: "Next.js Web3 Template",
+  description:
+    "This is a Next.js template to quick start building web3 apps with TypeScript, React, TailwindCSS, and Wagmi",
+  connectWallet: "Connect Wallet",
+};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+
+import type { AppProps } from "next/app";
+import { Inter } from "next/font/google";
+
+const fontSans = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className={`${fontSans.variable}`}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
